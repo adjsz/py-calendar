431ae646a9b56c07e345ed0e62fe27ce7db8d281
adjsz <robotdinosaur@outlook.com>
2026-03-08 20:56:09 +0000

    Added a new feature, preparing for release v1.1.0

--- py-calendar.py
+++ py-calendar.py
@@ -2,39 +2,63 @@ import datetime
 import calendar
 
 today = datetime.datetime.now()
-year = today.year
-month = today.month
+current_year = today.year
+current_month = today.month
 
 def show_date():
     formatted = today.strftime("\n%d %b %Y\n")
     print(formatted)
 
 def show_calendar():
-    print(calendar.month(year, month))
-    
+    print(calendar.month(current_year, current_month))
+
+def generate_calendar():
+    set_year = input("\nSet a year: ")
+    set_month = input("\nSet a month (1-12): ")
+
+    try:
+        set_year = int(set_year)
+        set_month = int(set_month)
+    except ValueError:
+          print("\nError: Please type numbers only.\n")
+          return
+
+    if set_month < 1 or set_month > 12:
+        print("\nError: Month can only be between 1 and 12.")
+        return
+
+    print("\n", calendar.month(set_year, set_month))
 
 def prompt():
     while True:
-        print("\nChoose an option:\n")
+        print("\nOptions:\n")
         print("1. Show today's date")
         print("2. Show today's calendar")
-        print("3. Show both\n")
-        user_input = input("")
-
+        print("3. Generate calendar")
+        print("4. Show both")
+        print("5. Exit\n")
+        user_input = input("Enter your choice: ")
+        
         try:
-            choice = int(user_input)
+                choice = int(user_input)
         except ValueError:
-            choice = user_input.lower()
+                print("\nError: invalid choice")
+                continue
 
         if choice == 1:
-            show_date()
+                show_date()
         elif choice == 2:
-            show_calendar()
+                show_calendar()
         elif choice == 3:
-            show_date()
-            show_calendar()
+                generate_calendar()
+        elif choice == 4:
+                show_date()
+                show_calendar()
+        elif choice == 5:
+                break # exits
         else:
-            print(f"\nError: '{choice}' is not a valid choice")
+                print("\nError: invalid choice")
+                continue
 
 
 def main():
